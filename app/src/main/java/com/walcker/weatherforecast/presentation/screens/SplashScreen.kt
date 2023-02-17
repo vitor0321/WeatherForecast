6e626e4963acc1a30a2ba05c16d353eab3a5587e
Vitor Walcker <walcker@MacBook-Pro-de-Vitor.local>
2023-02-17 19:54:43 -0300

    Feat: Splsh Screen Animation and Navigating to Main Screen

--- app/src/main/java/com/walcker/weatherforecast/presentation/screens/SplashScreen.kt
+++ app/src/main/java/com/walcker/weatherforecast/presentation/screens/SplashScreen.kt
@@ -1,5 +1,8 @@
 package com.walcker.weatherforecast.presentation.screens
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -11,8 +14,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
@@ -20,9 +26,25 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.walcker.weatherforecast.R
+import com.walcker.weatherforecast.navigation.WeatherScreens
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
+
+    val scale = remember { Animatable(0f) }
+
+
+    LaunchedEffect(key1 = true, block = {
+        scale.animateTo(
+            targetValue = 0.9f,
+            animationSpec = tween(durationMillis = 800,
+                easing = { OvershootInterpolator(8f).getInterpolation(it) }
+            )
+        )
+        delay(2000L)
+        navController.navigate(WeatherScreens.MainScreen.name)
+    })
 
     Surface(
         modifier = Modifier.fillMaxSize(),
@@ -32,6 +54,7 @@ fun SplashScreen(navController: NavHostController) {
             contentAlignment = Alignment.Center,
             modifier = Modifier
                 .fillMaxSize()
+                .scale(scale.value)
                 .background(
                     color = Color.Transparent,
                     shape = RoundedCornerShape(12.dp)
@@ -39,13 +62,11 @@ fun SplashScreen(navController: NavHostController) {
         ) {
 
             CircularProgressIndicator(
-                modifier= Modifier.size(290.dp),
+                modifier = Modifier.size(290.dp),
                 color = Color.Red,
             )
             Surface(
-                modifier = Modifier
-                    .padding(15.dp)
-                    .size(300.dp),
+                modifier = Modifier.size(300.dp),
                 shape = CircleShape,
                 color = Color.Transparent,
                 border = BorderStroke(
@@ -64,7 +85,7 @@ fun SplashScreen(navController: NavHostController) {
                         painter = painterResource(id = R.drawable.sun),
                         contentDescription = "Sunny icon",
                         contentScale = ContentScale.Fit,
-                        colorFilter = ColorFilter.tint(color= Color.Yellow)
+                        colorFilter = ColorFilter.tint(color = Color.Yellow)
                     )
                     Text(
                         text = "Find the Sun?",
